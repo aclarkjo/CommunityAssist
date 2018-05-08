91f1f21e2a5e309b7380c1a1ec1ad257b88246e7
Steve <steven.conger@seattlecolleges.edu>
2018-05-08 12:02:27 -0700

    Donation Assignment

---
+++ CommunityAssist/Controllers/DonationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist.Models;
+
+namespace CommunityAssist.Controllers
+{
+    public class DonationController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: Donation
+        public ActionResult Index()
+        {
+            if (Session["PersonKey"]== null)
+
+            {
+                Message m = new Message();
+                    m.Messagetext = "You must be logged in to add a Donation";
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index([Bind(Include = "PersonKey,DonationDate, DonationAmount,DonationConfirmationCode")]Donation b)
+        {
+
+            b.DonationDate = DateTime.Now;
+            b.PersonKey=(int) Session["PersonKey"];
+        b.DonationConfirmationCode=new Guid();
+            db.Donations.Add(b); 
+            db.SaveChanges();
+            Message m = new Message();
+            m.Messagetext = "Donation has been entered";
+            return View("Result", m);
+        }
+          
+    }
+}
